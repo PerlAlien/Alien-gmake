cc36b1fc4b92943eb8be2c5b3ac7cc411406b705
Graham Ollis <plicease@cpan.org>
2017-01-11 08:38:10 -0500

    Add probe for Alien::MSYS version of gmake
    building from source can fail on glob.h
    we get gmake from Alien::MSYS, and we need Alien::MSYS
    to build from source anyway, so maybe just use the
    version FROM Alien::gmake.
    
    Note that you will still get a build failure if you
    try to install with ALIEN_INSTALL_TYPE=share.
    Patches welcome.

--- inc/My/ModuleBuild.pm
+++ inc/My/ModuleBuild.pm
@@ -7,12 +7,26 @@ use Capture::Tiny qw( capture );
 use Config;
 use File::Temp qw( tempdir );
 use File::chdir;
+use File::Spec;
 
 sub alien_check_installed_version
 {
   my($self) = @_;
+
+  my @other;
   
-  foreach my $try ($Config{gmake}, $Config{make}, 'gmake', 'make')
+  if($^O eq 'MSWin32')
+  {
+    if($self->invoked_action ne 'Build_PL')
+    {
+      require Alien::MSYS;
+      my $path = File::Spec->catfile(Alien::MSYS::msys_path(), 'make');
+      $path =~ s/\\/\//g;
+      unshift @other, $path;
+    }
+  }
+  
+  foreach my $try ($Config{gmake}, $Config{make}, 'gmake', 'make', @other)
   {
     my($stdout, $stderr) = capture {
       system $try, '--version';
@@ -22,6 +36,15 @@ sub alien_check_installed_version
       my $version = $1;
       $self->config_data( gmake_called => $try );
       return $version;
+    }
+  }
+
+  if($^O eq 'MSWin32')
+  {
+    if($self->invoked_action eq 'Build_PL')
+    {
+      $self->_add_prereq( 'requires', 'Alien::MSYS', '0.07' );
+      return 'calculated later';
     }
   }
   
